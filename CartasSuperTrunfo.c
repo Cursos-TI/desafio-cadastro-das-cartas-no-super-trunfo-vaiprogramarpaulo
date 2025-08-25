6c8f12d32a009b2f5bd963ff1e98bd2b90b27d1f
vaiprogramarpaulo <studyinglikeabro@gmail.com>
2025-08-24 22:00:45 -0300

    Início

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,8 @@
 //Teste larissa
 
 int main() {
+
+    /
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
