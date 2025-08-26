1522e0d6c4e3cbd8440bb84147b8a715ec62bfdc
vaiprogramarpaulo <studyinglikeabro@gmail.com>
2025-08-26 07:00:36 -0300

    Remoção de informações desnecessárias

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -87,16 +87,5 @@ int main() {
     
     printf("\nEsse foi o jogo. Muito obrigado por jogarem! \n");
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     return 0;
 }
