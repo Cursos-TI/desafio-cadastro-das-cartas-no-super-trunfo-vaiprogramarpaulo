bfb450a66e08e09c98aebda0a1d25d42f2b6b765
vaiprogramarpaulo <studyinglikeabro@gmail.com>
2025-08-29 14:04:03 -0300

    Mudança de variaveis e remoção de comentários desnecessários

--- CartasSuperTrunfo_ComparandoCartas.c
+++ CartasSuperTrunfo_ComparandoCartas.c
@@ -7,31 +7,30 @@ int main() {
 
     char resposta; 
 
-    // Jogador 1
+    // Variaveis Jogador 1
     char nomeEstado1[50], nomeCidade1[50]; //Nome do Estado e da Cidade
     char codigoCarta1[4]; //Codigo da Carta (3 numeros + '\0')                  
     int populacao1, pontosturisticos1;
-    float area1, pib1, densidadepopulacional1, pibpercapita1;
+   float area1, pib1, densidadepopulacional1, pibpercapita1;
 
-    densidadepopulacional1 = populacao1 / area1; //Densidade Populacional = Populacao / Area
-    pibpercapita1 = pib1 / populacao1; //PIB per capita = PIB / Populacao
+    densidadepopulacional1 = populacao1 / area1;
+    pibpercapita1 = pib1 / populacao1; 
 
-    // Jogador 2
+    // Variaveis Jogador 2
     char nomeEstado2[50], nomeCidade2[50]; //Nome do Estado e da Cidade
     char codigoCarta2[4]; //Codigo da Carta (3 numeros + '\0')                  
     int populacao2, pontosturisticos2;
     float area2, pib2, densidadepopulacional2, pibpercapita2;
 
-    densidadepopulacional2 = populacao2 / area2; //Densidade Populacional = Populacao / Area
-    pibpercapita2 = pib2 / populacao2; //PIB per capita = PIB / Populacao
+    densidadepopulacional2 = populacao2 / area2; 
+    pibpercapita2 = pib2 / populacao2; 
     
 // Definidas as variaveis, vamos iniciar o código do jogo:
 
-    // = Jogador 1 =
     printf("= Seja bem-vindo ao Super Trunfo Paises! = \n\n");
     
     printf("Para iniciar, vamos criar a sua carta! \n\n");
-    printf("- Voce selecionou o Jogador 1. \n");
+    printf("- Voce selecionou o Jogador 1. \n"); // Criacao da carta do Jogador 1
 
     printf("Digite o código da sua carta (3 numeros): ");
     scanf("%i", &codigoCarta1);
@@ -63,6 +62,8 @@ int main() {
     printf("Area: %.2f\n km²", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos Turisticos: %d\n", pontosturisticos1);
+    printf("\nDensidade Populacional: %d\n", densidadepopulacional1);
+    printf("PIB per Capita: %.2f\n", pibpercapita1);
 
     printf("\n\nGostaria de avancar? (s/n): ");
     scanf(" %c", &resposta);
@@ -72,14 +73,15 @@ int main() {
         } else {
         printf("Vamos la! \n");
     }
-/*
-    // = Jogador 2 =
+
     printf("\nAgora e a vez do Jogador 2! \n\n");
-    printf("\n- Voce selecionou o Jogador 2. \n");
-    printf("O codigo da sua carta e 202!\n\n"); //Esse codigo sera ficticio, pois nao sera utilizado no jogo.
+    printf("\n- Voce selecionou o Jogador 2. \n"); // Criacao da carta do Jogador 2
+
+    printf("Digite o código da sua carta (3 numeros): ");
+    scanf("%i", &codigoCarta2);
     
-    printf("Digite a sigla do Estado: ");
-    scanf("%s", siglaEstado2);
+    printf("Digite o nome do Estado (sem espacos): ");
+    scanf("%s", nomeEstado2);
 
     printf("Digite o nome da cidade (sem espacos): ");
     scanf("%s", nomeCidade2);
@@ -95,16 +97,18 @@ int main() {
 
     printf("Digite o numero de pontos turisticos da sua cidade: ");
     scanf("%d", &pontosturisticos2);
-*/
+
     //Carta do Jogador 2
     printf("\n > Muito bem Jogador 2, esta e a sua carta:\n");
-    printf("Codigo da Carta: 202\n");
+    printf("Codigo da Carta: %i\n", codigoCarta2);
+    printf("Nome do Estado: %s\n", nomeEstado2);
     printf("Nome da Cidade: %s\n", nomeCidade2);
-    printf("Estado: %s\n", siglaEstado2);
-    printf("Populacao: %d\n", populacao2);
-    printf("Area: %.2f\n", area2);
+    printf("Populacao: %d mil habitantes\n", populacao2);
+    printf("Area: %.2f\n km²", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Pontos Turisticos: %d\n", pontosturisticos2);
+    printf("\nDensidade Populacional: %d\n", densidadepopulacional2);
+    printf("PIB per Capita: %.2f\n", pibpercapita2);
     
     printf("\n\nAgora vamos aos resultados! \n\n");
 
@@ -113,8 +117,7 @@ int main() {
     } else if (pib2 > pib1) {
         printf("O Jogador 2 venceu! \n");
     } else 
-    }
-    }
+    
 
     return 0;
 }
